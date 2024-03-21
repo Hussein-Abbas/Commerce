31f9b05ad1b54db35b242f9a9810159c2d1ff739
Hussein-Abbas <hussein.ihsan.abbas@gmail.com>
2024-03-21 22:52:50 +0300

    complete index page that renders active listings

--- auctions/views.py
+++ auctions/views.py
@@ -9,7 +9,10 @@ from .models import *
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = AuctionListing.objects.filter(status=True)
+    return render(request, "auctions/index.html", {
+        "listings": listings,
+    })
 
 
 def login_view(request):
